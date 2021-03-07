24a25df06d2b0aeb28a692e085ce8684be11165d
Kos8943 <a26095998@gmail.com>
2021-03-07 13:43:22 +0800

    檔案初始化

---
+++ Program/Work1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+
+namespace Work1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //如果args是空值
+            if (args.Length == 0)
+            {
+                Console.WriteLine("請輸入參數");
+                return;
+            }
+
+
+            //檔案路徑&名稱
+            string path = "D:\\C#\\AAA\\";
+            string file = "123.txt";
+
+
+            //如果檔案不存在
+            if(!File.Exists(path + file))
+            {
+                Console.WriteLine("檔案不存在");
+                return;
+            }
+
+
+            //將參數寫入TXT檔
+            foreach (string item in args)
+            {
+                File.AppendAllText(path + file, $"{item}{Environment.NewLine}");
+            }
+
+            
+            //讀取TXT檔
+            string readFile = File.ReadAllText(path + file);
+            Console.WriteLine(readFile);
+            
+            Console.ReadLine();
+
+
+        }
+    }
+}
